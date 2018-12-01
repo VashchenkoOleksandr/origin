225576fbaba50e75a8940e78c7c56a07976b106a
GeekHubUser <icqvkontakt@gmail.com>
2018-12-02 00:18:46 +0200

    Start learning JavaScript

---
+++ HW_Js1/test_MethodsProperty.js
@@ -0,0 +1,22 @@
+// Заготовка під тести:
+
+'use strict';
+test('test_string', () => {
+    expect().toEqual();
+});
+
+test('test_numbers', () => {
+    expect().toEqual();
+});
+
+test('test_arrays', () => {
+    expect().toEqual();
+});
+
+test('test_compare_operators', () => {
+    expect().toEqual();
+});
+
+test('test_equality_comparison_algorithm', () => {
+    expect().toEqual();
+});
